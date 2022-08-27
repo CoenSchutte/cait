d48087b65416c6350a924317d7481add2b723a80
Coen Schutte <coenschutte@gmail.com>
2022-08-27 21:58:24 +0200

    add invoice to nova

---
+++ app/Nova/Invoice.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Invoice extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Invoice::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make('Product', 'product')
+                ->required()
+                ->sortable(),
+
+            Text::make('Categorie', 'category')
+                ->required()
+                ->sortable(),
+
+            Select::make('Status', 'status')
+                ->options([
+                    'betaald' => 'Betaald',
+                    'afgeleverd' => 'Afgeleverd',
+                ])
+                ->displayUsingLabels()
+                ->required()
+                ->sortable(),
+
+            BelongsTo::make('User', 'user', User::class)
+                ->required()
+                ->sortable(),
+
+            Currency::make('Prijs', 'price')
+                ->currency('EUR')
+                ->required(),
+
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
