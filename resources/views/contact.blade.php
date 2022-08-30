a360f677aec6c72e35f9ad80decde43ac72fc6a6
Coen Schutte <coenschutte@gmail.com>
2022-08-30 13:14:23 +0200

    add ti lab room number

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,7 +21,6 @@
                 <div class="col-xl-9 mx-auto">
                     <iframe class="w-100 h-300" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2460.9075736425057!2d4.481728816170141!3d51.91739737970455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c4335dd6b0b5a3%3A0x3b8dcf047e6f0073!2sWijnhaven%20107%2C%203011%20WN%20Rotterdam!5e0!3m2!1sen!2snl!4v1661600468823!5m2!1sen!2snl" height="500" style="border:0;" aria-hidden="false" tabindex="0"></iframe>
 
-
                     <div class="row mt-5">
                         <div class="col-sm-6 mb-5 mb-sm-0">
                             <h3>Adverteren / Samenwerken</h3>
@@ -32,7 +31,7 @@
                         </div>
                         <div class="col-sm-6">
                             <h3>Contactinformatie </h3>
-                            <p>Loop even langs het TI-Lab, daar kun je eigenlijk altijd wel iemand vinden! Mocht er niemand zijn stuur dan even een berichtje op Teams</p>
+                            <p>Loop even langs het TI-Lab (ruimte wd.01.021), daar kun je eigenlijk altijd wel iemand vinden! Mocht er niemand zijn stuur dan even een berichtje op Teams</p>
                         </div>
                     </div>
                 </div>  <!-- Col END -->
