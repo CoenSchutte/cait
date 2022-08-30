c873b89e201501a20f0eea18f3cf6dcc6645caaa
Coen Schutte <coenschutte@gmail.com>
2022-08-30 15:08:30 +0200

    update mollie subscription method

--- app/Http/Controllers/CreateSubscriptionController.php
+++ app/Http/Controllers/CreateSubscriptionController.php
@@ -17,7 +17,7 @@ class CreateSubscriptionController extends Controller
 
         if(!$user->subscribed($name, $plan)) {
 
-            $result = $user->newSubscriptionViaMollieCheckout($name, $plan)->create();
+            $result = $user->newSubscription($name, $plan)->create();
 
             if(is_a($result, RedirectToCheckoutResponse::class)) {
                 return $result; // Redirect to Mollie checkout
