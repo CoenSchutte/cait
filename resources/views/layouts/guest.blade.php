192e70e7d0e2c0c088da2c32fd6c518fb99d1f85
Coen Schutte <coenschutte@gmail.com>
2022-08-30 13:24:07 +0200

    favicon auth pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="shortcut icon" href="../images/logo-stir-kleur.png">
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
