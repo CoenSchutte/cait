b594f7eb381d87eedf2d89daedb2259b8ce91b1e
Robin Martijn <ik@robinmartijn.nl>
2022-09-01 20:10:21 +0200

    🔥 remove unused import

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CreateSubscriptionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserEventRegistrationsController;
