c4bb4506d51fbc69906030b931c4368ddad225a0
Coen Schutte <coenschutte@gmail.com>
2022-12-13 16:51:35 +0100

    hide vacancies from activities show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
         $ad = Ad::where('expiration_date', '>=', today())->inRandomOrder()->first();
         if($ad) $ad->image_url = $ad->getSidebarAttribute();
 
-        $posts = Post::where('is_published', 1)->orderBy('created_at', 'desc')->paginate(6);
+        $posts = Post::where('is_published', 1)->whereNot('category', 'Vacature')->orderBy('created_at', 'desc')->paginate(6);
         $posts->map(function ($post) {
             $post->image_url = $post->get4by3Attribute();
             $post->low_res = $post->preview();
