505c08a47e2dd9925f0ce58d8f3370ebf3b22d81
Coen Schutte <coenschutte@gmail.com>
2023-09-04 10:06:34 +0200

    add register button to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,12 +34,18 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Wachtwoord vergeten?') }}
+            <div class="flex items-center justify-between mt-4">
+                <div class="flex flex-col">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Nog geen account?') }}
                     </a>
-                @endif
+
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                            {{ __('Wachtwoord vergeten?') }}
+                        </a>
+                    @endif
+                </div>
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
