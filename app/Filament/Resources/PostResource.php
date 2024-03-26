8d63f799a227d70084ec945401d22572d8c1703c
Coen Schutte <coenschutte@gmail.com>
2024-03-26 17:08:01 +0100

    add default post sorting

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -112,7 +112,7 @@ class PostResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array
