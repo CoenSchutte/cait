c8da6447e9f0d49b9121db70e86f9a1390bd7884
Coen Schutte <coenschutte@gmail.com>
2024-06-10 02:53:08 +0200

    add api auth

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
-
 use App\Http\Controllers\CreateSubscriptionController;
 use App\Http\Controllers\ProductController;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +28,82 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('paid', [CreateSubscriptionController::class, 'handleWebhookNotification'])->name('subscription.paid');
 Route::post('product/bought', [ProductController::class, 'handleWebhookNotification'])->name('products.paid');
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/test-csrf', fn () => [1, 2, 3]);
+
+    Route::post('logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    });
+
+
+});
+
+Route::post('/login', function(Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return response()->json([
+        'token' => $user->createToken($request->device_name)->plainTextToken,
+    ]);
+});
+
+Route::post('/register', function(Request $request) {
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required',
+        'student_number' => 'required|string|max:7',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'student_number' => $request->student_number,
+        'password' => Hash::make($request->password),
+    ]);
+
+    event(new Registered($user));
+
+    return response()->json([
+        'token' => $user->createToken($request->device_name)->plainTextToken,
+    ]);
+});
+
+Route::post('/forgot-password', function (Request $request) {
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+
+    if($status != Password::RESET_LINK_SENT) {
+        throw ValidationException::withMessages([
+            'email' => [__($status)]
+        ]);
+    }
+
+    return response()->json(['status' => __($status)]);
+
+});
+
 
 
 // group routes with prefix api
